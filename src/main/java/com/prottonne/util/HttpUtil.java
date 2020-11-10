dd0a7f1c0ea0fb2b1e3cf9908fb3c4e2fe729079
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2020-11-10 18:07:25 -0300

    fixes sonar vulnerability
    - fixes#16 -adds some constants to avoid duplication.

--- src/main/java/com/prottonne/util/HttpUtil.java
+++ src/main/java/com/prottonne/util/HttpUtil.java
@@ -19,8 +19,8 @@ public class HttpUtil {
     @Value("${http.util.url}")
     private String httpUtilUrl;
 
-    private final String OK = "Ok";
-    private final String NOT_OK = "Not Ok";
+    private static final String OK = "Ok";
+    private static final String NOT_OK = "Not Ok";
 
     protected String get(String paramName, Integer paramValue) {
 
@@ -35,9 +35,10 @@ public class HttpUtil {
             final String fullUrl = httpUtilUrl + "?" + paramName + "=" + paramValue;
             logger.info("check fullUrl {}", fullUrl);
 
-            if (!isUrlValid(fullUrl)) {
-                logger.info("url not valid");
-                return NOT_OK;
+            String urlWhiteListed = "https://";
+
+            if (!fullUrl.startsWith(urlWhiteListed)) {
+                throw new IOException();
             }
 
             URL url = new URL(fullUrl);
@@ -70,12 +71,6 @@ public class HttpUtil {
         logger.info(NOT_OK);
         return NOT_OK;
 
-    }
-
-    private boolean isUrlValid(String fullUrl) {
-        // TODO: we need to be sure url is valid
-        // A remote server making requests to URLs based on tainted data could enable attackers to make arbitrary requests to the internal network or to the local file system.
-        return fullUrl.length() < 60;
     }
 
 }
