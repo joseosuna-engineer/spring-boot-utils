41452d3ae748a89cf8ddbe5c21315c28421551fa
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2020-11-12 17:51:42 -0300

    adds a download binary file example
    - use HttpURLConnection to get the binary file from a third party server.
    - reads and puts into byte[]
    - sends this file back to whitting into OutputStream of HttpServletResponse
    - doesn't use the server to download the file from third party server
    - acts like a proxy to pass the bytes.

--- src/main/java/com/prottonne/util/HttpUtil.java
+++ src/main/java/com/prottonne/util/HttpUtil.java
@@ -1,10 +1,14 @@
 package com.prottonne.util;
 
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +19,9 @@ public class HttpUtil {
 
     @Value("${http.util.url}")
     private String httpUtilUrl;
+
+    @Value("${http.util.photo}")
+    private String httpUtilPhoto;
 
     private static final String OK = "Ok";
     private static final String NOT_OK = "Not Ok";
@@ -58,6 +65,59 @@ public class HttpUtil {
 
         logger.info(NOT_OK);
         return NOT_OK;
+
+    }
+
+    protected void download(HttpServletResponse response) {
+
+        final String filePath = "600/92c952";
+
+        HttpURLConnection connection = null;
+
+        try {
+
+            final String fullUrl = httpUtilPhoto + "/" + filePath;
+            logger.info("check fullUrl {}", fullUrl);
+
+            URL url = new URL(fullUrl);
+            logger.info("check path {}", url.getPath());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(RequestMethod.GET.name());
+            connection.connect();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+
+                byte[] image = IOUtils.toByteArray(connection.getInputStream());
+
+                if (image.length > 0) {
+
+                    try ( OutputStream out = response.getOutputStream()) {
+
+                        response.setContentLength(image.length);
+                        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+                        response.setHeader("Content-disposition", "attachment; filename=image.png");
+                        response.setStatus(HttpServletResponse.SC_OK);
+
+                        out.write(image);
+
+                    }
+
+                    logger.info(OK);
+
+                }
+
+            }
+
+        } catch (Exception ex) {
+            logger.error(NOT_OK, ex);
+        } finally {
+            if (null != connection) {
+                connection.disconnect();
+            }
+        }
+
+        logger.info(NOT_OK);
 
     }
 
